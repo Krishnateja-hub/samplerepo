3d6573f7afda3e2beac1f13b1a32d6b765e3073a
Krishna Teja sri <chkrishnatejasri1998@gmail.com>
2026-01-28 15:52:40 +0530

    python file

---
+++ reverse.py
@@ -0,0 +1,8 @@
+'''write a program to find the reverse of the number'''
+
+num=int(input('Enter a number:'))
+rev = 0
+while(num>0):
+    rev=rev*10+num%10
+    num//=10
+print('reverse number is',rev)    
