18675e8e36fd0702d5578934c6a44cf449345e6a
Krishna Teja sri <chkrishnatejasri1998@gmail.com>
2026-02-02 15:57:37 +0530

    story 20 added

--- reverse.py
+++ reverse.py
@@ -13,8 +13,24 @@ print(reverse(1236))
 print(ispalindrome(1236))
 
 print(reverse(123))
+<<<<<<< Updated upstream
 print(ispalindrome(1239))
 
+=======
+print(isPalindrome(123))
+>>>>>>> Stashed changes
 
 print(reverse(121))
 print(isPalindrome(121))
+
+def getPalindromes(start,end):
+    res=" "
+    for i in range(1,end+1):
+        if isPalindrome(i):
+            res=res+str(i)+","
+    return res
+print(getPalindromes(1,10000))        
+
+
+
+            
